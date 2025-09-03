0ced9a3797a5be30a979d41d37a3da8a200dbc09
Jonathan <jonathaanemanuel21@gmail.com>
2025-09-03 14:26:13 -0300

    add calculate imc route

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server := gin.Default()
@@ -8,6 +12,30 @@ func main() {
 	server.GET("hello-world", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "Hello, World!",
+		})
+	})
+
+	server.GET("functions/calculate-imc", func(ctx *gin.Context) {
+		height, err := strconv.ParseFloat(ctx.Query("height"), 64)
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"error": "Invalid height parameter",
+			})
+			return
+		}
+
+		weight, err := strconv.ParseFloat(ctx.Query("weight"), 64)
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"error": "Invalid weight parameter",
+			})
+			return
+		}
+
+		imc := weight / (height * height)
+
+		ctx.JSON(200, gin.H{
+			"result": imc,
 		})
 	})
 
